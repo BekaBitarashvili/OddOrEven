b0eeb14868537615a7c1595302d03cea8b04914c
BekaBitarashvili <bekabitarashvili@gmail.com>
2022-10-03 13:08:47 +0400

    Add project files.

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Please Enter Any Number: ");
+        int randomNumber = int.Parse(Console.ReadLine());
+
+
+        if (randomNumber % 2 == 0)
+        {
+            Console.WriteLine("Even");
+        }
+        else if (randomNumber % 2 == 1)
+        {
+            Console.WriteLine("Odd");
+        }
+        else if (randomNumber == 0)
+        {
+            Console.WriteLine("ZERO");
+        }
+}
